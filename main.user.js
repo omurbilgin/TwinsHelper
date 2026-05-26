73442b620cca8b1d3ab4a3345e08c9657edd7b86
omurbilgin <57267467+omurbilgin@users.noreply.github.com>
2026-05-26 19:14:12 +0300

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         main.user.js
+// @namespace    gh
+// @version      v01
+// @description  Helper script for Twins
+// @author       You
+// @match        http://10.237.5.169/dashboard
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=visualstudio.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
